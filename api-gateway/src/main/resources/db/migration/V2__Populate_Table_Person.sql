ed76e059dc55513807b0b0a6ebba32c189e2d801
everaldo.quintela <everaldo.quintela@trt19.jus.br>
2024-09-12 13:14:33 -0300

    Adicionadas migrations com Flyway

---
+++ api-gateway/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Maceió', 'João', 'Male', 'Correia'),
+	(2, 'Numseionde2 - Italy', 'Leonardo', 'Male', 'da Vinci'),
+	(5, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+	(7, 'Kentucky - USA', 'Muhammad', 'Male', 'Ali'),
+	(9, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(11, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
